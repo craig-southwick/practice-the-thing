652069f453ab01eba47e503a08dd9d7f12233fae
craig-southwick <112982273+craig-southwick@users.noreply.github.com>
2022-09-13 13:00:01 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,26 @@
-input.onGesture(Gesture.Shake, function () {
-    I_HUMAN_er_number = randint(0, 3)
-    if (I_HUMAN_er_number == 0) {
-        basic.showString("For the most wanted man in the sector you ain't that hard to find.")
+let Findly = 0
+let I_HUMAN_er_number = 0
+input.onButtonPressed(Button.A, function () {
+    Findly = randint(0, 1)
+    if (Findly == 0) {
+        basic.showString("Dare")
     }
-    if (I_HUMAN_er_number == 1) {
-        basic.showString("With a woman like that, there is only one thing TOO do.")
-    }
-    if (I_HUMAN_er_number == 2) {
-        basic.showString("40,000 gigawatts The power of the sun at your fingertips")
-    }
-    if (I_HUMAN_er_number == 3) {
-        basic.showString("I'm a bad bad man")
+    if (Findly == 1) {
+        basic.showString("Truth")
     }
 })
-let I_HUMAN_er_number = 0
-basic.showString("For random Tychus quote's shake.")
+input.onButtonPressed(Button.B, function () {
+    I_HUMAN_er_number = randint(0, 3)
+    if (I_HUMAN_er_number == 0) {
+        basic.showArrow(ArrowNames.South)
+    }
+    if (I_HUMAN_er_number == 1) {
+        basic.showArrow(ArrowNames.North)
+    }
+    if (I_HUMAN_er_number == 2) {
+        basic.showArrow(ArrowNames.East)
+    }
+    if (I_HUMAN_er_number == 3) {
+        basic.showArrow(ArrowNames.West)
+    }
+})
